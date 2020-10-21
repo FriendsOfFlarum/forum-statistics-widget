6912fffbb241a6b53326faca1f13b0747e275195
Ralkage <Ralkage@live.com>
2020-10-21 12:49:22 -0400

    Update for Flarum beta 14

--- js/src/admin/index.js
+++ js/src/admin/index.js
@@ -1,8 +1,8 @@
 import app from 'flarum/app';
 import WidgetSettings from './components/WidgetSettings';
 
-app.initializers.add('fof-forum-statistics-widget', app => {
-  app.extensionSettings['fof-forum-statistics-widget'] = () => {
-    app.modal.show(new WidgetSettings());
-  }
+app.initializers.add('fof-forum-statistics-widget', () => {
+    app.extensionSettings['fof-forum-statistics-widget'] = () => {
+        app.modal.show(WidgetSettings);
+    };
 });
--- js/src/forum/components/ForumStatisticsWidget.js
+++ js/src/forum/components/ForumStatisticsWidget.js
@@ -5,35 +5,37 @@ import formatNumber from 'flarum/utils/formatNumber';
 const translationPrefix = 'fof-forum-statistics-widget.forum.navbar.';
 
 export default class ForumStatisticsWidget extends Component {
-  init() {
-    super.init();
-  }
+    view() {
+        let discussionsCount = formatNumber(app.forum.attribute('discussionsCount'));
+        let postsCount = formatNumber(app.forum.attribute('postsCount'));
+        let usersCount = formatNumber(app.forum.attribute('usersCount'));
 
-  view() {
-    let discussionsCount = formatNumber(app.forum.attribute('discussionsCount'));
-    let postsCount = formatNumber(app.forum.attribute('postsCount'));
-    let usersCount = formatNumber(app.forum.attribute('usersCount'));
+        let user = {
+            displayName: m.prop(app.forum.attribute('lastUser')),
+            username: m.prop(app.forum.attribute('lastUser')),
+        };
 
-    let user = {
-      displayName: m.prop(app.forum.attribute('lastUser')),
-      username: m.prop(app.forum.attribute('lastUser')),
-    };
-
-    return (
-      <div class="ForumStatistics containerNarrow">
-        <div class="row">
-          <h2><i class="fas fa-chart-bar"></i> {app.translator.trans(translationPrefix + 'widget_title')}</h2>
-          <div>
-            <ul id="ForumStatisticsList">
-              <li>{app.translator.trans(translationPrefix + 'discussions_count', {discussionsCount})}</li>
-              <li>{app.translator.trans(translationPrefix + 'posts_count', {postsCount})}</li>
-              <li>{app.translator.trans(translationPrefix + 'users_count', {usersCount})}</li>
-              <li>{app.translator.trans(translationPrefix + 'latest_member')}
-                <a href={app.route.user(user)} config={m.route}><strong> {username(user)}</strong></a>
-              </li>
-            </ul>
-          </div>
-        </div>
-      </div>);
-  }
+        return (
+            <div class="ForumStatistics containerNarrow">
+                <div class="row">
+                    <h2>
+                        <i class="fas fa-chart-bar"></i> {app.translator.trans(translationPrefix + 'widget_title')}
+                    </h2>
+                    <div>
+                        <ul id="ForumStatisticsList">
+                            <li>{app.translator.trans(translationPrefix + 'discussions_count', { discussionsCount })}</li>
+                            <li>{app.translator.trans(translationPrefix + 'posts_count', { postsCount })}</li>
+                            <li>{app.translator.trans(translationPrefix + 'users_count', { usersCount })}</li>
+                            <li>
+                                {app.translator.trans(translationPrefix + 'latest_member')}
+                                <a href={app.route.user(user)} config={m.route}>
+                                    <strong> {username(user)}</strong>
+                                </a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        );
+    }
 }
