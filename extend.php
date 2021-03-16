7406ac22479cb971578e554e82b860db96e3c896
Ralkage <me@ralkage.com>
2021-03-15 22:35:03 -0400

    Update for beta 16

--- extend.php
+++ extend.php
@@ -3,7 +3,7 @@
 /*
  * This file is part of fof/forum-statistics-widget.
  *
- * Copyright (c) 2020 FriendsOfFlarum.
+ * Copyright (c) 2021 FriendsOfFlarum.
  *
  * For the full copyright and license information, please view the LICENSE.md
  * file that was distributed with this source code.
@@ -12,7 +12,11 @@
 namespace FoF\ForumStatisticsWidget;
 
 use Flarum\Api\Serializer\ForumSerializer;
+use Flarum\Discussion\Discussion;
 use Flarum\Extend;
+use Flarum\Post\Post;
+use Flarum\Settings\SettingsRepositoryInterface;
+use Flarum\User\User;
 
 return [
     (new Extend\Frontend('forum'))
@@ -21,8 +25,35 @@ return [
 
     (new Extend\Frontend('admin'))
         ->js(__DIR__ . '/js/dist/admin.js'),
+
     new Extend\Locales(__DIR__ . '/resources/locale'),
 
+    (new Extend\Settings())
+        ->serializeToForum(
+            'fof-forum-statistics-widget.ignore_private_discussions',
+            'fof-forum-statistics-widget.ignore_private_discussions',
+            'boolval'
+        ),
+
     (new Extend\ApiSerializer(ForumSerializer::class))
-        ->mutate(Listeners\AddForumAttributes::class),
+        ->attribute('fof-forum-statistics-widget.widget_order', function ($serializer, $model) {
+            $settings = resolve(SettingsRepositoryInterface::class);
+
+            return (int) $settings->get('fof-forum-statistics-widget.widget_order', 0);
+        })
+        ->attribute('fof-forum-statistics-widget.discussionsCount', function ($serializer, $model, $attributes) {
+            return $attributes['fof-forum-statistics-widget.ignore_private_discussions'] ?
+                Discussion::where('is_private', 0)->count() : Discussion::count();
+        })
+        ->attribute('fof-forum-statistics-widget.postsCount', function ($serializer, $model) {
+            return Post::where('type', 'comment')->count();
+        })
+        ->attribute('fof-forum-statistics-widget.usersCount', function ($serializer, $model) {
+            return User::count();
+        })
+        ->attribute('fof-forum-statistics-widget.lastUser', function ($serializer, $model) {
+            $lastUser = User::orderBy('joined_at', 'DESC')->limit(1)->first();
+
+            return $lastUser != null ? $lastUser->display_name : null;
+        }),
 ];
--- js/src/admin/index.js
+++ js/src/admin/index.js
@@ -1,6 +1,7 @@
 import app from 'flarum/app';
 
 const translationPrefix = 'fof-forum-statistics-widget.admin.settings.';
+const settingsPrefix = 'fof-forum-statistics-widget.';
 
 app.initializers.add('fof-forum-statistics-widget', () => {
     app.extensionData
@@ -8,16 +9,12 @@ app.initializers.add('fof-forum-statistics-widget', () => {
         .registerSetting({
             label: app.translator.trans(translationPrefix + 'widget_ignore_private_discussions_label'),
             setting: 'fof-forum-statistics-widget.ignore_private_discussions',
-            type: 'boolean',
+            type: 'switch',
         })
         .registerSetting({
             label: app.translator.trans(translationPrefix + 'widget_order_label'),
-            setting: 'fof-forum-statistics-widget.label1',
-            type: 'hidden',
-        })
-        .registerSetting({
-            label: app.translator.trans(translationPrefix + 'widget_helper_text'),
             setting: 'fof-forum-statistics-widget.widget_order',
             type: 'number',
+            help: app.translator.trans(translationPrefix + 'widget_helper_text'),
         });
 });
