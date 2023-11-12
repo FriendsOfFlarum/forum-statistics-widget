046f3852f810d6cf964f867dec432401fd53449d
Ian Morland <ian@morland.me>
2023-11-12 09:00:43 +0000

    fix: stats queried on every request

--- js/src/forum/index.ts
+++ js/src/forum/index.ts
@@ -3,9 +3,10 @@ import { extend } from 'flarum/common/extend';
 import IndexPage from 'flarum/forum/components/IndexPage';
 import ForumStatisticsWidget from './components/ForumStatisticsWidget';
 import ItemList from 'flarum/common/utils/ItemList';
+import type Mithril from 'mithril';
 
 app.initializers.add('fof/forum-statistics-widget', () => {
-  extend(IndexPage.prototype, 'sidebarItems', function (items: ItemList) {
+  extend(IndexPage.prototype, 'sidebarItems', function (items: ItemList<Mithril.Children>) {
     items.add('forumStatisticsWidget', ForumStatisticsWidget.component(), app.forum.attribute('fof-forum-statistics-widget.widget_order'));
   });
 });
