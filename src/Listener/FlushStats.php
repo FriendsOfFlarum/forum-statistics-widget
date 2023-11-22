a838cbed4a8fad95bb5d58382e2283a9060e5ebb
IanM <16573496+imorland@users.noreply.github.com>
2023-11-22 09:42:09 +0000

    fix: flush cache on user delete (#29)
    * fix: flush cache on user delete
    
    * Apply fixes from StyleCI
    
    ---------
    
    Co-authored-by: StyleCI Bot <bot@styleci.io>

---
+++ src/Listener/FlushStats.php
@@ -0,0 +1,42 @@
+<?php
+
+/*
+ * This file is part of fof/forum-statistics-widget.
+ *
+ * Copyright (c) FriendsOfFlarum.
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace FoF\ForumStatisticsWidget\Listener;
+
+use Flarum\User\Event\Deleted as UserDeleted;
+use Flarum\User\Event\Deleting as UserDeleting;
+use FoF\ForumStatisticsWidget\AddForumStats;
+use Illuminate\Contracts\Cache\Repository as Cache;
+use Illuminate\Contracts\Events\Dispatcher;
+
+class FushStats
+{
+    /**
+     * @var Cache
+     */
+    public $cache;
+
+    public function __construct(Cache $cache)
+    {
+        $this->cache = $cache;
+    }
+
+    public function subscribe(Dispatcher $events): void
+    {
+        // Flush on both deleting and deleted else a request make in between deleting and deleted will return the old stats.
+        $events->listen([UserDeleting::class, UserDeleted::class], [$this, 'flushStats']);
+    }
+
+    public function flushStats($event): void
+    {
+        $this->cache->forget(AddForumStats::CACHE_KEY);
+    }
+}
